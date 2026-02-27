32c2f6539e61414e076136cb072ec2e092bbd0a2
tedarrioja-sketch <ted.arrioja@gmail.com>
2026-02-27 10:40:43 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'powertimer-v8';
+const CACHE = 'powertimer-v9';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
   self.skipWaiting();
